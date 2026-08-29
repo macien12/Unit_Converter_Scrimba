ba199a8d5eeeba6aef27a83ae13d93056d138bda
macien12 <maciejnaw@pm.me>
2026-08-29 19:27:15 +0200

    Function now using parameter

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ const literCount = 0.264
 const kilogramCount = 2.204
 
 convertBtn.addEventListener("click", function() {
-    convertNumber()
+    convertNumber(inputEl.value)
 })
 
-function convertNumber() {
-    lengthEl.innerHTML = `<p> ${inputEl.value} meters = ${(inputEl.value * meterCount).toFixed(3)} feet | ${inputEl.value} feet = ${(inputEl.value / meterCount).toFixed(3)} meters</p>`
+function convertNumber(value) {
+    lengthEl.innerHTML = `<p> ${value} meters = ${(value * meterCount).toFixed(3)} feet | ${value} feet = ${(value / meterCount).toFixed(3)} meters</p>`
     
-    volumeEl.innerHTML = `<p> ${inputEl.value} liters = ${(inputEl.value * literCount).toFixed(3)} gallons | ${inputEl.value} gallons = ${(inputEl.value / literCount).toFixed(3)} liters</p>`
+    volumeEl.innerHTML = `<p> ${value} liters = ${(value * literCount).toFixed(3)} gallons | ${value} gallons = ${(value / literCount).toFixed(3)} liters</p>`
     
-    massEl.innerHTML = `<p> ${inputEl.value} kilos = ${(inputEl.value * kilogramCount).toFixed(3)} pounds | ${inputEl.value} pounds = ${(inputEl.value / kilogramCount).toFixed(3)} kilos</p>`
+    massEl.innerHTML = `<p> ${value} kilos = ${(value * kilogramCount).toFixed(3)} pounds | ${value} pounds = ${(value / kilogramCount).toFixed(3)} kilos</p>`
 }
 
 
